7a10cd4cf730ad57c658ca232c48369cb1d2ac13
devsTz <rogashianmvungi@gmail.com>
2024-01-10 23:31:54 +0300

    Class and Object

---
+++ JavaClass.java
@@ -0,0 +1,10 @@
+public class JavaClass {
+    int x =5;
+    public static void main(String[] args){
+        JavaClass myObj1 = new JavaClass();
+        JavaClass myObj2 = new JavaClass();
+
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
