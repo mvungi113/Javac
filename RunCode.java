91bebcaaf4b2bb7007523d5f69eec861109c4bea
devsTz <rogashianmvungi@gmail.com>
2024-01-10 23:02:07 +0300

    Java Basic

---
+++ RunCode.java
@@ -0,0 +1,5 @@
+public class RunCode {
+    // step 01 Open the command line cmd or terminal for linux
+    //  step 02 navgate to the directory where you save your code
+    //  step 03 javac filename.java
+}
