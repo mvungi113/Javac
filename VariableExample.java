829fcd6b55d934323c10ca1157dc8d9e5bc8653e
devsTz <rogashianmvungi@gmail.com>
2024-01-10 23:07:53 +0300

    Java Basic variable

---
+++ VariableExample.java
@@ -0,0 +1,12 @@
+public class VariableExample {
+    public static void main(String[] args){
+        int num;
+        num =10;
+        double doubleNum;
+        doubleNum = 5.5;
+        boolean isJavaFun = true;
+        System.out.println (num);
+        System.out.println (doubleNum);
+        System.out.println (isJavaFun);
+    }
+}
